4edbb2ca278ea973fb1571e2888c5830b22114c1
Dimitrij P <d.phoursa@outlook.com>
2014-11-27 16:40:24 +0000

    Added solution to Euler's 39th problem

---
+++ integer_right_triangles.js
@@ -0,0 +1,42 @@
+/**
+ *
+ * Integer right triangles
+ *
+ * Problem 39
+ *
+ * If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
+ *
+ * {20,48,52}, {24,45,51}, {30,40,50}
+ *
+ * For which value of p ² 1000, is the number of solutions maximised?
+ *
+ */
+
+// c = sqrt(a^2 + b^2)
+// P = a + b + c
+// P = a + b + sqrt(a^2 + b^2)
+
+var answers = [];
+
+var answer = 0;
+var answer_p = 0;
+
+for (var a = 1; a < 500; a++) {
+    for (var b = 1; b < 500; b++) {
+        var p = a + b + Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        if (p <= 1000) {
+            if (typeof(answers[p]) == "undefined") {
+                answers[p] = 0;
+            }
+            
+            answers[p]++;
+            
+            if (answer < answers[p]) {
+                answer_p = p;
+                answer = answers[p];
+            }
+        }
+    }
+}
+
+console.log("Answer " + answer_p);
